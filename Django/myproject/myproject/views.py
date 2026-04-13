81b789d0182f41827464ee288c5d34b1357747c1
Jeevan Kumar <jeevankumarguduru3@gmail.com>
2026-04-13 15:16:32 +0530

    Remove Django directory from git tracking

--- Django/myproject/myproject/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-def hello(request):
-    return HttpResponse("Hello Django")
-
-def bye(request):
-    return HttpResponse("Byee...")
-
-# home, name, address, education
