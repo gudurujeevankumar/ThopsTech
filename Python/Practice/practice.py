aaf81909578b055cd4f6176058caafd8444f3b5c
Jeevan Kumar <jeevankumarguduru3@gmail.com>
2026-04-13 15:24:08 +0530

    Frequency of a number program

--- Python/Practice/practice.py
+++ Python/Practice/practice.py
@@ -1,3 +1,20 @@
 # write a program to print most frequency of a number 
 # least repeated number
 
+nums = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5]
+
+freq = {}
+
+for num in nums:
+    if num in freq:
+        freq[num] += 1
+    else:
+        freq[num] = 1
+
+print("Frequency:", freq)
+
+max_num = max(freq, key=freq.get)
+min_num = min(freq, key=freq.get)
+
+print("Most frequent number:", max_num)
+print("Least frequent number:", min_num)
